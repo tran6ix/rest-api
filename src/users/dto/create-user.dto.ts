83cb11b30a149ddc5ab28620c6b5180bbc91402a
Jordan <jvt2h@mtmail.mtsu.edu>
2024-03-13 23:25:32 -0500

    initial commit

---
+++ src/users/dto/create-user.dto.ts
@@ -0,0 +1,6 @@
+export class CreateUserDto {
+  public firstName: string;
+  public lastName: string;
+  public email: string;
+  public phoneNumber: string;
+}
---
+++ src/users/entities/user.entity.ts
@@ -0,0 +1,19 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column('text')
+  firstName: string;
+
+  @Column('text')
+  lastName: string;
+
+  @Column('text')
+  email: string;
+
+  @Column('text')
+  phoneNumber: string;
+}
